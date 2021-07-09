d55c02a25b718116a0dd6a3400b6f2a8d6e5b2fd
agnostic-me <official.ks00@gmail.com>
2021-07-09 16:24:08 +0530

    implement send welcome email functionality after user sign up

--- swiftly/settings.py
+++ swiftly/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # project apps
-    'src',
+    # 'src', commented as using apps.py
+    'src.apps.SrcConfig',
 
     # 3rd party apps
     'bootstrap4',
@@ -153,3 +154,13 @@ SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
     'fields': 'id, name, email'
 }
+
+
+# configuring SMTP
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT", default=25, cast=int)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default='localhost')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
