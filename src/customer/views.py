ca357d88d2fc1689a842c54ef9443f5c5ab8c27a
agnostic-me <official.ks00@gmail.com>
2021-07-14 18:38:45 +0530

    add phone number model & functionality and its verification using firebase

--- src/customer/views.py
+++ src/customer/views.py
@@ -1,3 +1,4 @@
+from swiftly.settings import FIREBASE_PRIVATE_KEY_ID
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
@@ -7,7 +8,28 @@ from django.contrib import messages
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
 
-# Create your views here.
+from django.conf import settings
+
+import firebase_admin
+from firebase_admin import credentials, auth
+
+# Firebase Configuration
+cred = credentials.Certificate({
+    "type": settings.FIREBASE_TYPE,
+    "project_id": settings.FIREBASE_PROJECT_ID,
+    "private_key_id": settings.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": settings.FIREBASE_PRIVATE_KEY,
+    "client_email": settings.FIREBASE_CLIENT_EMAIL,
+    "client_id": settings.FIREBASE_CLIENT_ID,
+    "auth_uri": settings.FIREBASE_AUTH_URI,
+    "token_uri": settings.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": settings.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": settings.FIREBASE_CLIENT_X509_CERT_URL,
+})
+firebase_admin.initialize_app(cred)
+
+
+# write your views here
 
 @login_required()
 def home(request):
@@ -40,10 +62,26 @@ def profile_page(request):
                 messages.success(request, 'Your Password has been updated successfully!')
                 return redirect(reverse('customer:profile'))
 
+        elif request.POST.get('action') == 'update_phone':
+            # Get Firebase user data
+            firebase_user = auth.verify_id_token(request.POST.get('id_token'))
+
+            request.user.customer.phone_number = firebase_user['phone_number']
+            request.user.customer.save()
+            return redirect(reverse('customer:profile'))
+
     context = {
         'user_form': user_form,
         'customer_form': customer_form,
         'change_password_form': change_password_form,
+
+        # firebase configuration
+        'FIREBASE_API_KEY': settings.FIREBASE_API_KEY,
+        'FIREBASE_AUTH_DOMAIN': settings.FIREBASE_AUTH_DOMAIN,
+        'FIREBASE_PROJECT_ID': settings.FIREBASE_PROJECT_ID,
+        'FIREBASE_STORAGE_BUCKET': settings.FIREBASE_STORAGE_BUCKET,
+        'FIREBASE_MESSAGING_SENDER_ID': settings.FIREBASE_MESSAGING_SENDER_ID,
+        'FIREBASE_APP_ID': settings.FIREBASE_APP_ID,
     }
     return render(request, 'customer/profile.html', context)
     
